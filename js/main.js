0c9fcb82bfbee684b9cac16aa33644043a98fa76
Dmitry <dmitronus2015@gmail.com>
2024-11-20 13:06:07 +0200

    add about block

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(document).ready(function () {
+  $(".popup__link").magnificPopup({
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  });
 });
